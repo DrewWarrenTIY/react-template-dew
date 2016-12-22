91dbf2054f69cb1492a3633a3357cf038c9dd9bd
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-12-22 10:26:21 -0700

    Create working template for React App

---
+++ src/app/components/App.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import React from 'react';
+import { BroswerRouter as Router, Link, Match, Miss } from 'react-router';
+
+import styles from '../styles/App.css';
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <h1>You are rendering!</h1>
+    )
+  }
+}
